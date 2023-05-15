1ce2c47cb02d60fe07e8b910142ae77499dc8938
Rizky Jayusman <rizky.jayusman96@gmail.com>
2023-05-15 13:27:42 +0700

    add demo endpoint

---
+++ demo/src/main/java/com/rizkyjayusman/demo/DemoController.java
@@ -0,0 +1,16 @@
+package com.rizkyjayusman.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+    }
+
+}
